6a076a51378bb3fdec5c8b522c1fd7e014a7d23a
Claudio Mendonca <claudioccm@gmail.com>
2025-05-22 08:42:07 -0700

    setting up 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,21 @@
-const postcss = require('postcss');
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
+  // Plugins
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  
+  // Passthrough copies
   eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/css");
 
+  // Filters
+  eleventyConfig.addFilter("date", function(date, format) {
+    if (format === "yyyy") {
+      return new Date().getFullYear();
+    }
+    return new Date(date).toLocaleDateString();
+  });
+  
   return {
     dir: {
       input: "src",
